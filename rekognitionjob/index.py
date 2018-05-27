b1244380f794502fb85ff78b260b461e1e9ea794
Sam Van Overmeire <svanovermeire@branch.be>
2018-05-27 16:56:09 +0200

    Add rekognition handler.

--- rekognitionjob/index.py
+++ rekognitionjob/index.py
@@ -1,8 +1,32 @@
+import os
+import boto3
+from helpers import s3_helper
+
+s3_client = boto3.client('s3')
+rek_client = boto3.client('rekognition')
 
 
 def my_handler(event, context):
-    job_name = 'rek-job-for'
+    bucket, key = s3_helper.extract_bucket_and_key_from_event(event)
+    job_name = 'rek-job-for-' + key
+
+    # TODO use ClientRequestToken
+    response = rek_client.start_celebrity_recognition(
+        Video={
+            'S3Object': {
+                'Bucket': bucket,
+                'Name': key
+            }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REK_SNS'],
+            'RoleArn': os.environ['REK_ROLE']
+        },
+        JobTag=job_name
+    )
+
+    print('Job id is ' + response['JobId'])
 
     return {
-        'message': 'started job for name ' + job_name
+        'message': 'started job for ' + job_name
     }
